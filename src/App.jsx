f46f0336455ea16c37a07574eb9841947b41f620
saha-7op <saha7pritam@gmail.com>
2025-07-19 12:51:33 +0530

    Routing Setup, Outlet, Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Login from "./components/Login"
+import Body from "./components/Body"
+import Profile from "./components/Profile"
 
 
 function App() {
 
   return (
     <>
-      <h1>DevFinder</h1>
+    <BrowserRouter basename="/">
+    <Routes>
+      <Route path="/" element={<Body />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+      
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
