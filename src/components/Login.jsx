2c8a0783274e2812aed59f0e35ec4bcda9fdac71
saha-7op <saha7pritam@gmail.com>
2025-07-22 10:19:30 +0530

    Login UI & axios setup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,107 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  )
+  const [emailId, setEmailId] = useState("")
+  const [password, setPassword] = useState("");
+  const handlelogin = async() =>{
+    try{
+      const res =await axios.post("http://localhost:3000/login",{
+        emailId,
+        password
+      })
+    }catch(err){
+      console.error("Login failed", err);
+  }
 }
+  return (
+    <div className="flex justify-center my-20">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title flex justify-center">Login</h2>
+          <div>
+            <label className="input validator ">
+              <svg
+                className="h-[1em] opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                  <circle cx="12" cy="7" r="4"></circle>
+                </g>
+              </svg>
+              <input
+                type="text"
+                value={emailId}
+                onChange={(e)=>setEmailId(e.target.value)}
+                required
+                placeholder="Username"
+                pattern="[A-Za-z][A-Za-z0-9\-]*"
+                minlength="3"
+                maxlength="30"
+                title="Only letters, numbers or dash"
+              />
+            </label>
+            <p className="validator-hint">
+              Must be 3 to 30 characters
+              <br />
+              containing only letters, numbers or dash
+            </p>
+            <label className="input validator">
+              <svg
+                className="h-[1em] opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                  <circle
+                    cx="16.5"
+                    cy="7.5"
+                    r=".5"
+                    fill="currentColor"
+                  ></circle>
+                </g>
+              </svg>
+              <input
+                type="password"
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                required
+                placeholder="Password"
+                minlength="8"
+                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+              />
+            </label>
+            <p className="validator-hint hidden">
+              Must be more than 8 characters, including
+              <br />
+              At least one number <br />
+              At least one lowercase letter <br />
+              At least one uppercase letter
+            </p>
+          </div>
+          <div className="card-actions justify-center my-2">
+            <button className="btn btn-primary" onClick={handlelogin}>Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
