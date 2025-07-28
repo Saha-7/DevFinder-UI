76cbaee4b9ada68f5ec54035842cbed6c78feda3
saha-7op <saha7pritam@gmail.com>
2025-07-28 10:30:55 +0530

    connectionSlice

---
+++ src/components/Connections.jsx
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { BASE_URL } from '../utils/constants'
+
+const Connections = () => {
+  const fetchConnections = async()=>{
+    try{
+      const res = await axios.get(BASE_URL + "/user/connections",{withCredentials:true})
+      console.log(res?.data?.data);
+      
+    }catch(err){
+      console.log(err);
+    }
+  }
+  useEffect(()=>{
+    fetchConnections()
+  },[])
+  return (
+    <div>Connections</div>
+  )
+}
+
+export default Connections
