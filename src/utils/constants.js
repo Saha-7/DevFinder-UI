6eeb25ef33533b41ee782b2620368c62b97f2fa0
saha-7op <saha7pritam@gmail.com>
2025-08-07 18:48:10 +0530

    Dynamic url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,6 @@
-export const BASE_URL = "/api";
+// Dynamic URL
+export const BASE_URL = location.hostname === "localhost"? "http://localhost:3000" : "/api";
+
+//Local URL
 
 // export const BASE_URL="http://localhost:3000"
