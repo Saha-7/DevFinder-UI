bc985dc45540d46394ce9c4bf56cb749b383692d
saha-7op <saha7pritam@gmail.com>
2025-08-25 22:26:58 +0530

    Chat design

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -6,8 +6,17 @@ const Chat = () => {
     console.log(targetUserId);
     
   return (
-    <div>
-      
+    <div className='flex flex-col w-1/2 mx-auto border border-gray-100 rounded-lg m-5 h-[75vh]'>
+      <h1 className='p-5 border-b border-gray-100'>Chat</h1>
+
+      <div className='flex-1 overflow-scroll p-5'>
+        {/* Display Message */}
+      </div>
+
+      <div className='p-5 border-t border-gray-100 flex items-center gap-2'>
+        <input placeholder='Type here...' className='bg-black text-white flex-1 border-gray-100 p-2 rounded-lg'/>
+        <button className='btn-secondary btn'>Send</button>
+      </div>
     </div>
   )
 }
