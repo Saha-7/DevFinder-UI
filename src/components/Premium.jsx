6f5f645219e18e334aee69bc18016f8505109c05
saha-7op <saha7pritam@gmail.com>
2025-09-16 19:52:42 +0530

    Ui almost done

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -2,26 +2,26 @@
 const Premium = () => {
   return (
     <div className="m-10 ">
-      <div className="flex space-x-7 w-full flex-col lg:flex-row">
+      <div className="flex space-x-7 w-2/3 flex-col lg:flex-row">
         <div className="card bg-base-300 rounded-box grid h-56 grow place-items-center">
-          <h1 className="font-bold">Silver</h1>
+          <h1 className="font-extrabold text-2xl text-amber-100">Platinum</h1>
           <ul>
             <li> - Unlimited chat per day</li>
             <li> - 50 connection requests per day</li>
             <li> - Blue tick</li>
             <li> - 3 months validity</li>
           </ul>
-          <button className="bg-slate-400 text-black m-2 p-2 rounded-lg">Select</button>
+          <button className="bg-slate-400 text-black m-2 p-2 px-4 rounded-lg">Buy</button>
         </div>
         <div className="card bg-base-300 rounded-box grid h-56 grow place-items-center">
-          <h1 className="font-bold">Gold</h1>
+          <h1 className="font-extrabold text-2xl text-amber-500">Gold</h1>
           <ul>
             <li> - Unlimited chat per day</li>
             <li> - 100 connection requests per day</li>
             <li> - Blue tick</li>
             <li> - 6 months validity</li>
           </ul>
-          <button className="bg-slate-400 text-black m-2 p-2 rounded-lg">Select</button>
+          <button className="bg-yellow-500 text-black m-2 p-2 px-4 rounded-lg">Buy</button>
         </div>
       </div>
     </div>
