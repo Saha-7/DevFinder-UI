2f50a1c51d2b2ca6a7225d5d4b252582236d03aa
saha-7op <saha7pritam@gmail.com>
2025-09-16 19:27:57 +0530

    membership component added

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Premium = () => {
   return (
-    <div>
-      Premium
+    <div className="m-10 ">
+      <div className="flex space-x-7 w-full flex-col lg:flex-row">
+        <div className="card bg-base-300 rounded-box grid h-32 grow place-items-center">
+          content
+        </div>
+        <div className="card bg-base-300 rounded-box grid h-32 grow place-items-center">
+          content
+        </div>
+        <div className="card bg-base-300 rounded-box grid h-32 grow place-items-center">
+          content
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Premium
+export default Premium;
