6e1de70604287b5404c4d63b5a06d297ebd0e66c
saha-7op <saha7pritam@gmail.com>
2025-09-16 19:41:55 +0530

    feat:) membership plan details

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,17 +1,27 @@
-import React from "react";
 
 const Premium = () => {
   return (
     <div className="m-10 ">
       <div className="flex space-x-7 w-full flex-col lg:flex-row">
-        <div className="card bg-base-300 rounded-box grid h-32 grow place-items-center">
-          content
+        <div className="card bg-base-300 rounded-box grid h-56 grow place-items-center">
+          <h1 className="font-bold">Silver</h1>
+          <ul>
+            <li> - Unlimited chat per day</li>
+            <li> - 50 connection requests per day</li>
+            <li> - Blue tick</li>
+            <li> - 3 months validity</li>
+          </ul>
+          <button className="bg-slate-400 text-black m-2 p-2 rounded-lg">Select</button>
         </div>
-        <div className="card bg-base-300 rounded-box grid h-32 grow place-items-center">
-          content
-        </div>
-        <div className="card bg-base-300 rounded-box grid h-32 grow place-items-center">
-          content
+        <div className="card bg-base-300 rounded-box grid h-56 grow place-items-center">
+          <h1 className="font-bold">Gold</h1>
+          <ul>
+            <li> - Unlimited chat per day</li>
+            <li> - 100 connection requests per day</li>
+            <li> - Blue tick</li>
+            <li> - 6 months validity</li>
+          </ul>
+          <button className="bg-slate-400 text-black m-2 p-2 rounded-lg">Select</button>
         </div>
       </div>
     </div>
