9b221382bbe40c52498c052ea1009adff89dd8cb
saha-7op <saha7pritam@gmail.com>
2025-09-17 13:11:05 +0530

    ui fixed for web screen

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,9 +1,15 @@
 
 const Premium = () => {
+
+  const handleBuyClick = async(type)=>{
+
+  }
+
+
   return (
     <div className="m-10 flex justify-center items-center h-96">
       <div className="flex space-x-7 w-2/3 flex-col lg:flex-row">
-        <div className="card bg-base-300 rounded-box grid h-72 grow place-items-center transition-transform duration-300 hover:-translate-y-2">
+        <div className="card bg-base-300 rounded-box grid h-72 w-1/2 grow place-items-center transition-transform duration-300 hover:-translate-y-2">
           <h1 className="font-extrabold text-2xl text-amber-100">Platinum</h1>
           <ul>
             <li> - Unlimited chat per day</li>
@@ -11,9 +17,9 @@ const Premium = () => {
             <li> - Blue tick</li>
             <li> - 3 months validity</li>
           </ul>
-          <button className="bg-slate-400 text-black m-2 p-2 px-4 rounded-lg cursor-pointer">Buy</button>
+          <button className="bg-amber-50 text-black m-2 p-2 px-4 rounded-lg cursor-pointer">Buy</button>
         </div>
-        <div className="card bg-base-300 rounded-box grid h-72 grow place-items-center transition-transform duration-300 hover:-translate-y-2">
+        <div className="card bg-base-300 rounded-box grid h-72 w-1/2 grow place-items-center transition-transform duration-300 hover:-translate-y-2">
           <h1 className="font-extrabold text-2xl text-amber-500">Gold</h1>
           <ul>
             <li> - Unlimited chat per day</li>
@@ -21,7 +27,7 @@ const Premium = () => {
             <li> - Blue tick</li>
             <li> - 6 months validity</li>
           </ul>
-          <button className="bg-yellow-500 text-black m-2 p-2 px-4 rounded-lg cursor-pointer">Buy</button>
+          <button onClick={()=>handleBuyClick("gold")} className="bg-yellow-500 text-black m-2 p-2 px-4 rounded-lg cursor-pointer">Buy</button>
         </div>
       </div>
     </div>
