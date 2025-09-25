89b4ec662e965d752a239de1ce0e407ed964203a
saha-7op <saha7pritam@gmail.com>
2025-09-25 20:33:22 +0530

    Basic chat setup

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const Chat = () => {
     const {targetUserId} = useParams()
     console.log(targetUserId);
+    const [messages, setMessages] = useState([{text: "Hello World"}, {text: "Hello World 2"}])
     
   return (
     <div className='flex flex-col w-1/2 mx-auto border border-gray-100 rounded-lg m-5 h-[75vh]'>
@@ -11,6 +12,22 @@ const Chat = () => {
 
       <div className='flex-1 overflow-scroll p-5'>
         {/* Display Message */}
+        {messages.map((msg,index)=>{
+          return(
+            <>
+            <div key={index} className="chat chat-start">
+              <div className="chat-bubble">
+                It's over Anakin,
+                <br />
+                I have the high ground.
+              </div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-bubble">You underestimate my power!</div>
+            </div>
+            </>
+          )
+        })}
       </div>
 
       <div className='p-5 border-t border-gray-100 flex items-center gap-2'>
