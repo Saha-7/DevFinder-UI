6fa604ec0afd7859b834a35bb6001cdce457ef4f
saha-7op <saha7pritam@gmail.com>
2025-09-25 22:21:44 +0530

    Pass user data through URL state

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 const Chat = () => {
+
+  const location = useLocation()
+
   const { targetUserId } = useParams();
   console.log(targetUserId);
+
+  // Receving end user
+  const ChatUser = location.state?.user
+  console.log(ChatUser);
   const [messages, setMessages] = useState([
     { text: "Hello World" },
     { text: "Hello World 2" },
@@ -23,19 +30,26 @@ const Chat = () => {
                   It's over Anakin,
                   <br />I have the high ground.
                   <span className="align-sub text-[10px] opacity-50 ml-2">
-      12:46
-      <img
-        alt="double tick"
-        className="inline-block ml-1 w-3 h-3 align-sub invert hue-rotate-180"
-        src="https://img.icons8.com/ios-glyphs/30/double-tick--v1.png"
-      />
-    </span>
+                    12:46
+                    <img
+                      alt="double tick"
+                      className="inline-block ml-1 w-3 h-3 align-sub invert hue-rotate-180"
+                      src="https://img.icons8.com/ios-glyphs/30/double-tick--v1.png"
+                    />
+                  </span>
                 </div>
               </div>
               <div className="chat chat-end">
                 <div className="chat-bubble">
                   You underestimate my power!
-                  <time className="text-xs opacity-50 ml-3">12:46</time>
+                  <span className="align-sub text-[10px] opacity-50 ml-2">
+                    12:46
+                    <img
+                      alt="double tick"
+                      className="inline-block ml-1 w-3 h-3 align-sub invert hue-rotate-180"
+                      src="https://img.icons8.com/ios-glyphs/30/double-tick--v1.png"
+                    />
+                  </span>
                 </div>
               </div>
             </>
