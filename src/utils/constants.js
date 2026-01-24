10a1793fa2da250af90a1bcb8d3cb92103a9390b
Pritam Saha <saha7pritam@gmail.com>
2026-01-24 21:33:30 +0530

    Update BASE_URL to include production URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,11 @@
 // Dynamic URL
-export const BASE_URL = location.hostname === "localhost"? "http://localhost:3000" : "/api";
+// export const BASE_URL = location.hostname === "localhost"? "http://localhost:3000" : "/api";
 
 //Local URL
 
 // export const BASE_URL="http://localhost:3000"
+
+
+export const BASE_URL = location.hostname === "localhost"
+  ? "http://localhost:3000" 
+  : "https://devfinder-4ah0.onrender.com";
